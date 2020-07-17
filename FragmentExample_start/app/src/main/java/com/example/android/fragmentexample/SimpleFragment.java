5e2576fb91eadba60b86dbe7b560602456e75c44
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-17 11:48:21 -0400

    Initialized RadioGroup and set onClick listener to change Fragment Header with what the user chooses

--- FragmentExample_start/app/src/main/java/com/example/android/fragmentexample/SimpleFragment.java
+++ FragmentExample_start/app/src/main/java/com/example/android/fragmentexample/SimpleFragment.java
@@ -5,8 +5,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class SimpleFragment extends Fragment {
+
+    private static final int YES = 0;
+    private static final int NO = 1;
 
 
     public SimpleFragment() {
@@ -18,6 +23,28 @@ public class SimpleFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_simple, container, false);
+        final View rootView =  inflater.inflate(R.layout.fragment_simple, container, false);
+        final RadioGroup radioGroup = rootView.findViewById(R.id.radio_group);
+
+        radioGroup.OnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+                View radioButton = radioGroup.findViewById(checkedId);
+                int index = radioGroup.indexOfChild(radioButton);
+                TextView textView = rootView.findViewById(R.id.fragment_header);
+                switch (index) {
+                    case YES:
+                        textView.setText(R.string.yes_message);
+                        break;
+                    case NO:
+                        textView.setText(R.string.no_message);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+
+        return rootView;
     }
 }
