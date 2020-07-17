cd1d822e70aee973f39a1bb2c216168d451fb15d
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-17 12:28:38 -0400

    Initialized Open Button in MainActivity

--- FragmentExample_start/app/src/main/java/com/example/android/fragmentexample/MainActivity.java
+++ FragmentExample_start/app/src/main/java/com/example/android/fragmentexample/MainActivity.java
@@ -18,12 +18,18 @@ package com.example.android.fragmentexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+    private boolean isFragmentDisplayed = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton = findViewById(R.id.open_button);
     }
 }
