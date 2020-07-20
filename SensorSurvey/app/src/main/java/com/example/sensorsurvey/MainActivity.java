c3706fc3a44caf5fba9cae536b803173440ac008
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-20 10:47:30 -0400

    Initialized SensorManager in MainActivity. Got a list of sensors and set text of ScrollView item to name

--- SensorSurvey/app/src/main/java/com/example/sensorsurvey/MainActivity.java
+++ SensorSurvey/app/src/main/java/com/example/sensorsurvey/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.sensorsurvey;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SensorManager mSensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        List<Sensor> sensorList = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+        StringBuilder sensorText = new StringBuilder();
+        for (Sensor current : sensorList) {
+            sensorText.append(current.getName()).append(System.getProperty("line.separator"));
+        }
+
+        TextView sensorTextView = (TextView)findViewById(R.id.sensor_list);
+        sensorTextView.setText(sensorText);
+
     }
 }
