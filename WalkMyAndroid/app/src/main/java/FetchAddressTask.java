71df797634afc931f254ccc82fb8e088e2f8309c
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-21 12:09:02 -0400

    Added try, catch to get list of addresses with geocoder

--- WalkMyAndroid/app/src/main/java/FetchAddressTask.java
+++ WalkMyAndroid/app/src/main/java/FetchAddressTask.java
@@ -1,6 +1,15 @@
 import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.AsyncTask;
+import android.util.Log;
+
+import com.example.android.walkmyandroid.R;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class FetchAddressTask extends AsyncTask<Location, Void, String> {
 
@@ -13,6 +22,17 @@ public class FetchAddressTask extends AsyncTask<Location, Void, String> {
 
     @Override
     protected String doInBackground(Location... locations) {
+        Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+        Location location = locations[0];
+        List<Address> addresses = null;
+        String resultMessage = "";
+        try {
+            addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(),1);
+        } catch (IOException e) {
+            e.printStackTrace();
+            resultMessage = context.getString(R.string.service_not_available);
+            Log.e(TAG, resultMessage, e);
+        }
         return null;
     }
 
