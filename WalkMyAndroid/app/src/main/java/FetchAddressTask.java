dabc25a1a8195c0cb92c81d411d572446ae3961a
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-21 11:59:47 -0400

    Created new FetchAddressTask class that extends AsyncTask

---
+++ WalkMyAndroid/app/src/main/java/FetchAddressTask.java
@@ -0,0 +1,11 @@
+import android.location.Location;
+import android.os.AsyncTask;
+
+public class FetchAddressTask extends AsyncTask<Location, Void, String> {
+
+
+    @Override
+    protected String doInBackground(Location... locations) {
+        return null;
+    }
+}
