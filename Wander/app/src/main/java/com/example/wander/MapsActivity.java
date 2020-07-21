f5142c6340626187691ed192881c9b12822b4a7e
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-21 17:49:42 -0400

    Changed marker style

--- Wander/app/src/main/java/com/example/wander/MapsActivity.java
+++ Wander/app/src/main/java/com/example/wander/MapsActivity.java
@@ -14,6 +14,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
@@ -109,7 +110,9 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                         latLng.latitude,
                         latLng.longitude);
 
-                map.addMarker(new MarkerOptions().position(latLng).title(getString(R.string.dropped_pin)).snippet(snippet));
+                map.addMarker(new MarkerOptions().position(latLng).title(getString(R.string.dropped_pin)).snippet(snippet).icon(BitmapDescriptorFactory.defaultMarker
+                        (BitmapDescriptorFactory.HUE_BLUE)));
+
             }
         });
     }
