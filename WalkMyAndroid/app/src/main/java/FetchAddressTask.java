de130b462223a791b7e96ed48581f6dd163cdbc7
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-21 12:02:34 -0400

    Created Context and TAG variables. Overrode shells for doInBackground and onPostExecute

--- WalkMyAndroid/app/src/main/java/FetchAddressTask.java
+++ WalkMyAndroid/app/src/main/java/FetchAddressTask.java
@@ -1,11 +1,23 @@
+import android.content.Context;
 import android.location.Location;
 import android.os.AsyncTask;
 
 public class FetchAddressTask extends AsyncTask<Location, Void, String> {
 
+    private Context context;
+    private static final String TAG = FetchAddressTask.class.getSimpleName();
+
+    public FetchAddressTask(Context context) {
+        this.context = context;
+    }
 
     @Override
     protected String doInBackground(Location... locations) {
         return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
     }
 }
