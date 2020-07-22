c8cdfa070f136ec8642be6d94259026742fe27c1
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-22 11:00:50 -0400

    Added constructors and created init, showClearButton and hideClearButton shells

--- CustomEditText/app/src/main/java/com/example/customedittext/EditTextWithClear.java
+++ CustomEditText/app/src/main/java/com/example/customedittext/EditTextWithClear.java
@@ -1,21 +1,41 @@
 package com.example.customedittext;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 
 import androidx.appcompat.widget.AppCompatEditText;
+import androidx.core.content.res.ResourcesCompat;
 
 public class EditTextWithClear extends AppCompatEditText {
 
+    Drawable mClearButtonImage;
+
     public EditTextWithClear(Context context) {
         super(context);
+        init();
     }
 
     public EditTextWithClear(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public EditTextWithClear(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        mClearButtonImage = ResourcesCompat.getDrawable(getResources(),
+                R.drawable.ic_clear_opaque_24, null);
+    }
+
+    private void showClearButton() {
+
+    }
+
+    private void hideClearButton() {
+        
     }
 }
