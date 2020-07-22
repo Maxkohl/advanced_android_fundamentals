e7e9cc585be525015bcf4f377d3416299ee1e06c
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-22 14:22:58 -0400

    Instantiated member variables

--- SimpleCanvas/app/src/main/java/com/example/simplecanvas/MainActivity.java
+++ SimpleCanvas/app/src/main/java/com/example/simplecanvas/MainActivity.java
@@ -2,9 +2,31 @@ package com.example.simplecanvas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Canvas mCanvas;
+    private Paint mPaint = new Paint();
+    private Paint mPaintText = new Paint(Paint.UNDERLINE_TEXT_FLAG);
+    private Bitmap mBitMap;
+    private ImageView mImageView;
+
+    private Rect mRect = new Rect();
+    private Rect mBounds = new Rect();
+
+    private static final int OFFSET = 120;
+    private int mOffset = OFFSET;
+    private static final int MULTIPLIER = 100;
+
+    private int mColorBackground;
+    private int mColorRectangle;
+    private int mColorAccent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
