1f45d0bdc230953463afa3da83095dfdb81f1a20
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-22 11:08:43 -0400

    Added textChangeListener and OnTouchListener to init

--- CustomEditText/app/src/main/java/com/example/customedittext/EditTextWithClear.java
+++ CustomEditText/app/src/main/java/com/example/customedittext/EditTextWithClear.java
@@ -2,7 +2,11 @@ package com.example.customedittext;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
 
 import androidx.appcompat.widget.AppCompatEditText;
 import androidx.core.content.res.ResourcesCompat;
@@ -29,13 +33,44 @@ public class EditTextWithClear extends AppCompatEditText {
     private void init() {
         mClearButtonImage = ResourcesCompat.getDrawable(getResources(),
                 R.drawable.ic_clear_opaque_24, null);
+
+        addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                showClearButton();
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if ((getCompoundDrawablesRelative()[2] != null)) {
+                    float clearButtonstart;
+                    float clearButtonEnd;
+                    boolean isClearButtonClicked = false;
+                }
+                return false;
+            }
+        });
     }
 
     private void showClearButton() {
+        setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, mClearButtonImage, null);
 
     }
 
     private void hideClearButton() {
-        
+        setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, null, null);
     }
 }
