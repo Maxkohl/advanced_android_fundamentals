82ab4d4dad2bf4afc871c90632301e7a28e47c5c
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-22 10:56:34 -0400

    Created EditTextWithClear class and extended EditText

---
+++ CustomEditText/app/src/main/java/com/example/customedittext/EditTextWithClear.java
@@ -0,0 +1,21 @@
+package com.example.customedittext;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatEditText;
+
+public class EditTextWithClear extends AppCompatEditText {
+
+    public EditTextWithClear(Context context) {
+        super(context);
+    }
+
+    public EditTextWithClear(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public EditTextWithClear(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
