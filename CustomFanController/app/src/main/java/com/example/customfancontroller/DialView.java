3a7760a91e075ae32c3836435eb3a9555c9b192b
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-22 12:02:03 -0400

    Created DialView class and extended ImageView

---
+++ CustomFanController/app/src/main/java/com/example/customfancontroller/DialView.java
@@ -0,0 +1,20 @@
+package com.example.customfancontroller;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatImageView;
+
+public class DialView extends AppCompatImageView {
+    public DialView(Context context) {
+        super(context);
+    }
+
+    public DialView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DialView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
