b3556770cb355c36026185ee14962814cf16dea7
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-22 17:56:34 -0400

    Cleaned up code

--- PropertyAnimation/app/src/main/java/com/example/propertyanimation/PulseAnimationView.java
+++ PropertyAnimation/app/src/main/java/com/example/propertyanimation/PulseAnimationView.java
@@ -3,49 +3,50 @@ package com.example.propertyanimation;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.os.Build;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.animation.AccelerateInterpolator;
 import android.view.animation.LinearInterpolator;
 
 import androidx.annotation.Nullable;
+import androidx.interpolator.view.animation.LinearOutSlowInInterpolator;
 
 
 public class PulseAnimationView extends View {
-    private float mRadius;
-    private final Paint mPaint = new Paint();
     private static final int COLOR_ADJUSTER = 5;
     private AnimatorSet mPulseAnimatorSet = new AnimatorSet();
 
     private float mX;
     private float mY;
 
+    private float mRadius;
+    private final Paint mPaint = new Paint();
+
     private static final int ANIMATION_DURATION = 4000;
     private static final long ANIMATION_DELAY = 1000;
 
 
-    public PulseAnimationView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-    }
+    public PulseAnimationView(Context context) {this(context, null);}
 
-    public PulseAnimationView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+    public PulseAnimationView(Context context, AttributeSet attrs) {
+        super(context, attrs);
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
         ObjectAnimator growAnimator = ObjectAnimator.ofFloat(this, "radius", 0, getWidth());
-        growAnimator.setDuration(ANIMATION_DURATION).setInterpolator(new AccelerateInterpolator());
+        growAnimator.setDuration(ANIMATION_DURATION);
+        growAnimator.setInterpolator(new LinearInterpolator());
 
         ObjectAnimator shrinkAnimator = ObjectAnimator.ofFloat(this, "radius", getWidth(), 0);
-        shrinkAnimator.setDuration(ANIMATION_DURATION).setInterpolator(new LinearInterpolator());
+        shrinkAnimator.setDuration(ANIMATION_DURATION);
+        shrinkAnimator.setInterpolator(new LinearOutSlowInInterpolator());
         shrinkAnimator.setStartDelay(ANIMATION_DELAY);
 
         ObjectAnimator repeatAnimator = ObjectAnimator.ofFloat(this,
@@ -60,6 +61,7 @@ public class PulseAnimationView extends View {
 
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
@@ -70,21 +72,20 @@ public class PulseAnimationView extends View {
                 mPulseAnimatorSet.cancel();
             }
             mPulseAnimatorSet.start();
-
         }
         return super.onTouchEvent(event);
     }
-
-    public void setRadius(float radius) {
-        this.mRadius = radius;
-        mPaint.setColor(Color.GREEN + (int) mRadius / COLOR_ADJUSTER);
-        invalidate();
-    }
-
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.drawCircle(mX, mY, mRadius, mPaint);
     }
+
+    public void setRadius(float radius) {
+        this.mRadius = radius;
+        mPaint.setColor(Color.GREEN + (int) radius / COLOR_ADJUSTER);
+        invalidate();
+    }
+
 }
