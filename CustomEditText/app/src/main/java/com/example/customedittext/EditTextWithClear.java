f66361a7024aca1b4d3d75c1c571e6beffcebbd1
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-22 11:16:37 -0400

    Added condition to check for RTL or LTR layout and position clearImageButton accordingly. Added conditional to trigger clearing of EditText when user clicks button

--- CustomEditText/app/src/main/java/com/example/customedittext/EditTextWithClear.java
+++ CustomEditText/app/src/main/java/com/example/customedittext/EditTextWithClear.java
@@ -56,9 +56,46 @@ public class EditTextWithClear extends AppCompatEditText {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 if ((getCompoundDrawablesRelative()[2] != null)) {
-                    float clearButtonstart;
+                    float clearButtonStart;
                     float clearButtonEnd;
                     boolean isClearButtonClicked = false;
+
+                    if (getLayoutDirection() == LAYOUT_DIRECTION_RTL) {
+                        // If RTL, get the end of the button on the left side.
+                        clearButtonEnd = mClearButtonImage
+                                .getIntrinsicWidth() + getPaddingStart();
+                        // If the touch occurred before the end of the button,
+                        // set isClearButtonClicked to true.
+                        if (motionEvent.getX() < clearButtonEnd) {
+                            isClearButtonClicked = true;
+                        }
+                    } else {
+                        // Layout is LTR.
+                        // Get the start of the button on the right side.
+                        clearButtonStart = (getWidth() - getPaddingEnd()
+                                - mClearButtonImage.getIntrinsicWidth());
+                        // If the touch occurred after the start of the button,
+                        // set isClearButtonClicked to true.
+                        if (motionEvent.getX() > clearButtonStart) {
+                            isClearButtonClicked = true;
+                        }
+                    }
+                    if (isClearButtonClicked) {
+                        if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                            mClearButtonImage = ResourcesCompat.getDrawable(getResources(),
+                                    R.drawable.ic_clear_black_24, null);
+                            showClearButton();
+                        }
+                        if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                            mClearButtonImage = ResourcesCompat.getDrawable(getResources(),
+                                    R.drawable.ic_clear_opaque_24, null);
+                            getText().clear();
+                            hideClearButton();
+                            return true;
+                        }
+                    } else {
+                        return false;
+                    }
                 }
                 return false;
             }
