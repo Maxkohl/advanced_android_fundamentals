038f65481e3593a160ae50b5366cd60c8e128ce9
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-22 14:58:03 -0400

    Added dimensions

--- ClippingExample/app/src/main/java/com/example/clippingexample/ClippedView.java
+++ ClippingExample/app/src/main/java/com/example/clippingexample/ClippedView.java
@@ -1,12 +1,17 @@
 package com.example.clippingexample;
 
 import android.content.Context;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 public class ClippedView extends View {
+    private Paint mPaint;
+    private Path mPath;
+
     public ClippedView(Context context) {
         super(context);
     }
