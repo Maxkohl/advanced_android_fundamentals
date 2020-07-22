4a58cc4ea79aac25e4d3b72b79bc95c2f9653973
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-22 12:07:21 -0400

    Created init method and implemented in constructors

--- CustomFanController/app/src/main/java/com/example/customfancontroller/DialView.java
+++ CustomFanController/app/src/main/java/com/example/customfancontroller/DialView.java
@@ -1,19 +1,48 @@
 package com.example.customfancontroller;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class DialView extends View {
+    private static int SELECTION_COUNT = 4; // Total number of selections.
+    private float mWidth;                   // Custom view width.
+    private float mHeight;                  // Custom view height.
+    private Paint mTextPaint;               // For text in the view.
+    private Paint mDialPaint;               // For dial circle in the view.
+    private float mRadius;                  // Radius of the circle.
+    private int mActiveSelection;           // The active selection.
+    // String buffer for dial labels and float for ComputeXY result.
+    private final StringBuffer mTempLabel = new StringBuffer(8);
+    private final float[] mTempResult = new float[2];
+
     public DialView(Context context) {
         super(context);
+        init();
     }
 
     public DialView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public DialView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mTextPaint.setColor(Color.BLACK);
+        mTextPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        mTextPaint.setTextAlign(Paint.Align.CENTER);
+        mTextPaint.setTextSize(40f);
+        mDialPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mDialPaint.setColor(Color.GRAY);
+        // Initialize current selection.
+        mActiveSelection = 0;
+        // TODO: Set up onClick listener for this view.
     }
 }
