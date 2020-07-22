2ceb5ae8b6742eb58d3fec0db17a528196132902
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-22 15:28:27 -0400

    Added constructor, getters and update method to FlashlightCone class

--- SurfaceViewExample/app/src/main/java/com/example/surfaceviewexample/FlashlightCone.java
+++ SurfaceViewExample/app/src/main/java/com/example/surfaceviewexample/FlashlightCone.java
@@ -5,5 +5,26 @@ public class FlashlightCone {
     private int mY;
     private int mRadius;
 
-    
+    public FlashlightCone(int viewWidth, int viewHeight) {
+        this.mX = viewWidth / 2;
+        this.mY = viewHeight / 2;
+        mRadius = ((viewWidth <= viewHeight) ? mX / 3 : mY / 3);
+    }
+
+    public void update(int newX, int newY) {
+        mX = newX;
+        mY = newY;
+    }
+
+    public int getmX() {
+        return mX;
+    }
+
+    public int getmY() {
+        return mY;
+    }
+
+    public int getmRadius() {
+        return mRadius;
+    }
 }
