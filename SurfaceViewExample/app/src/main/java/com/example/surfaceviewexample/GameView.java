ed69b6115406969b8ed952e5afe36ef37c8d09ab
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-22 15:50:09 -0400

    Created setUpBitmap method

--- SurfaceViewExample/app/src/main/java/com/example/surfaceviewexample/GameView.java
+++ SurfaceViewExample/app/src/main/java/com/example/surfaceviewexample/GameView.java
@@ -1,8 +1,14 @@
 package com.example.surfaceviewexample;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
 import android.os.Build;
 import android.util.AttributeSet;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import androidx.annotation.RequiresApi;
@@ -10,6 +16,15 @@ import androidx.annotation.RequiresApi;
 public class GameView extends SurfaceView implements Runnable {
 
     private Context mContext;
+    private SurfaceHolder mSurfaceHolder;
+    private Paint mPaint;
+    private Path mPath;
+    private int mViewWidth;
+    private int mViewHeight;
+    private Bitmap mBitMap;
+    private Bitmap mBitmapX;
+    private Bitmap mBitmapY;
+    private Bitmap mWinnerRect;
 
     public GameView(Context context) {
         super(context);
@@ -43,5 +58,24 @@ public class GameView extends SurfaceView implements Runnable {
 
     public void init(Context context) {
         mContext = context;
+        mSurfaceHolder = getHolder();
+        mPaint = new Paint();
+        mPaint.setColor(Color.DKGRAY);
+        mPath = new Path();
+
     }
+
+    private void setUpBitmap() {
+        mBitmapX = (int) Math.floor(
+                Math.random() * (mViewWidth - mBitmap.getWidth()));
+        mBitmapY = (int) Math.floor(
+                Math.random() * (mViewHeight - mBitmap.getHeight()));
+        mWinnerRect = new RectF(mBitmapX, mBitmapY,
+                mBitmapX + mBitmap.getWidth(),
+                mBitmapY + mBitmap.getHeight());
+    }
+
+    
+
+
 }
