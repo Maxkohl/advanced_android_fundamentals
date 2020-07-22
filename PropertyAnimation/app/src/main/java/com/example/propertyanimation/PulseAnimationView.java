5a0765760748a0ec679ab1d3ceaa1dabf8a3ba76
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-22 16:59:22 -0400

    Created setRadius setter to change color and size of animation

--- PropertyAnimation/app/src/main/java/com/example/propertyanimation/PulseAnimationView.java
+++ PropertyAnimation/app/src/main/java/com/example/propertyanimation/PulseAnimationView.java
@@ -1,6 +1,8 @@
 package com.example.propertyanimation;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
@@ -8,7 +10,13 @@ import android.view.View;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 
+
 public class PulseAnimationView extends View {
+    private float mRadius;
+    private final Paint mPaint = new Paint();
+    private static final int COLOR_ADJUSTER = 5;
+
+
     public PulseAnimationView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
@@ -17,4 +25,9 @@ public class PulseAnimationView extends View {
         super(context, attrs, defStyleAttr);
     }
 
+    public void setRadius(float mRadius) {
+        this.mRadius = mRadius;
+        mPaint.setColor(Color.GREEN + (int) mRadius / COLOR_ADJUSTER);
+        invalidate();
+    }
 }
