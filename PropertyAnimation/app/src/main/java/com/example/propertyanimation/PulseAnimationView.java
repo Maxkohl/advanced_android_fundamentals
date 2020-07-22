5f1b0b27b187a3ac8ea85f6979034268ce075280
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-22 16:52:19 -0400

    Created PulseAnimationView class and added it to activity_main.xml

---
+++ PropertyAnimation/app/src/main/java/com/example/propertyanimation/PulseAnimationView.java
@@ -0,0 +1,20 @@
+package com.example.propertyanimation;
+
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+
+public class PulseAnimationView extends View {
+    public PulseAnimationView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PulseAnimationView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
