1ce5ce1c7a751ad0492164007fe462d2ccc3f598
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-23 11:51:18 -0400

    Saved mCurrentPosition to instance bundle and reinitialized in onCreate to handle orientation changes

--- SimpleVideoView/app/src/main/java/com/example/simplevideoview/MainActivity.java
+++ SimpleVideoView/app/src/main/java/com/example/simplevideoview/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.simplevideoview;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
@@ -11,16 +12,23 @@ import android.widget.VideoView;
 public class MainActivity extends AppCompatActivity {
     private static final String VIDEO_SAMPLE = "tacoma_narrows";
     private VideoView mVideoView;
+    private int mCurrentPosition = 0;
+    private static final String PLAYBACK_TIME = "play_time";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (savedInstanceState != null) {
+            mCurrentPosition = savedInstanceState.getInt(PLAYBACK_TIME);
+        }
+
         mVideoView = findViewById(R.id.videoview);
         MediaController controller = new MediaController(this);
         controller.setMediaPlayer(mVideoView);
         mVideoView.setMediaController(controller);
+
     }
 
     private Uri getMedia(String mediaName) {
@@ -30,6 +38,11 @@ public class MainActivity extends AppCompatActivity {
     private void initializePlayer() {
         Uri videoUri = getMedia(VIDEO_SAMPLE);
         mVideoView.setVideoURI(videoUri);
+        if (mCurrentPosition > 0) {
+            mVideoView.seekTo(mCurrentPosition);
+        } else {
+            mVideoView.seekTo(1);
+        }
         mVideoView.start();
     }
 
@@ -55,7 +68,13 @@ public class MainActivity extends AppCompatActivity {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
             mVideoView.pause();
         }
+        mCurrentPosition = mVideoView.getCurrentPosition();
     }
 
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(PLAYBACK_TIME, mCurrentPosition);
 
+    }
 }
