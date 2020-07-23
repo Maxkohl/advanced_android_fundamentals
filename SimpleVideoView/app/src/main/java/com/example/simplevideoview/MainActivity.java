3a0a9ee405949e73b8ec11b4c9529d05f38f3cde
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-23 11:08:53 -0400

    Created getMedia and initializePlayer methods

--- SimpleVideoView/app/src/main/java/com/example/simplevideoview/MainActivity.java
+++ SimpleVideoView/app/src/main/java/com/example/simplevideoview/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.simplevideoview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String VIDEO_SAMPLE = "tacoma_narrows";
+    private VideoView mVideoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mVideoView = findViewById(R.id.videoview);
+    }
+
+    private Uri getMedia(String mediaName) {
+        return Uri.parse("android.resource://" + getPackageName() + "/raw/" + mediaName);
+    }
+
+    private void initializePlayer() {
+
     }
 }
